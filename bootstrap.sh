928a0de99d75bd9716326118182e267d2d6c0250
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2013-04-10 19:57:16 +0200

    Updated with commonrc to load from zshrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,8 @@ cd ~
 
 ditto ~/.dotfiles/.vim ~/.vim
 cp ~/.dotfiles/.vimrc ~/.vimrc
-cp ~/.dotfiles/.bash_prompt ~/.bash_prompt
-cp ~/.dotfiles/.exports ~/.exports
 cp ~/.dotfiles/.aliases ~/.aliases
-cp ~/.dotfiles/.functions ~/.functions
 cp ~/.dotfiles/.git-completion ~/.git-completion
-cp ~/.dotfiles/.bash_profile ~/.bash_profile
+cp ~/.dotfiles/.commonrc ~/.commonrc
 
 echo "Don't forget to fix your .gitconfig!"
